116175fa930e7bbfe2e4859a92d105df28edffa0
christian-hopf <christian-hopf@cognizant.com>
2024-08-16 20:47:15 -0400

    Sticky SteamInput with slightly transparent bg

--- src/components/stats/Achievements.jsx
+++ src/components/stats/Achievements.jsx
@@ -2,7 +2,40 @@ import React from "react";
 
 function Achievements() {
   return (
-    <div className="flex flex-col mt-8">
+    <div className="flex flex-col gap-8 mt-8">
+      <div className="flex flex-row w-96 bg-black rounded-md bg-opacity-70">
+        <img
+          className="px-6 py-6"
+          src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/570940/b3e4ca8626661bf2a67b7c207dae62088927bba7.jpg"
+          alt=""
+        />
+        <div className="flex flex-col mt-2">
+          <h1 className="text-xl text-white font-semibold">Enkindle</h1>
+          <h2 className="text-white">Light bonfire flame.</h2>
+        </div>
+      </div>
+      <div className="flex flex-row w-96 bg-black rounded-md bg-opacity-70">
+        <img
+          className="px-6 py-6"
+          src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/570940/b3e4ca8626661bf2a67b7c207dae62088927bba7.jpg"
+          alt=""
+        />
+        <div className="flex flex-col mt-2">
+          <h1 className="text-xl text-white font-semibold">Enkindle</h1>
+          <h2 className="text-white">Light bonfire flame.</h2>
+        </div>
+      </div>
+      <div className="flex flex-row w-96 bg-black rounded-md bg-opacity-70">
+        <img
+          className="px-6 py-6"
+          src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/570940/b3e4ca8626661bf2a67b7c207dae62088927bba7.jpg"
+          alt=""
+        />
+        <div className="flex flex-col mt-2">
+          <h1 className="text-xl text-white font-semibold">Enkindle</h1>
+          <h2 className="text-white">Light bonfire flame.</h2>
+        </div>
+      </div>
       <div className="flex flex-row w-96 bg-black rounded-md bg-opacity-70">
         <img
           className="px-6 py-6"
