8ec4ca01fc9390a1ae525db85bdd2fbf4d6ec01e
christian-hopf <christian-hopf@cognizant.com>
2024-08-18 21:05:55 -0400

    Fixed padding on input header

--- src/components/input-form/SteamInput.jsx
+++ src/components/input-form/SteamInput.jsx
@@ -6,7 +6,7 @@ function SteamInput() {
     // get achievements
   }
   return (
-    <form className="flex flex-row sticky top-0 gap-6 justify-center items-center bg-black bg-opacity-70 py-8 pb-4">
+    <form className="flex flex-row sticky top-0 gap-6 justify-center items-center bg-black bg-opacity-70 py-4">
       <div className="flex flex-row gap-2 items-center">
         <h2 className="text-white">SteamID</h2>
         <input
