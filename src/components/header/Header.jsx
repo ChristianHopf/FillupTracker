aad99ebb5ee8005af6a5054ec954e17fc86d9450
christian-hopf <christian-hopf@cognizant.com>
2024-08-19 21:57:19 -0400

    Header, StatsCard, Achievements receive and display real data

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import mockData from "../../mock/getplayersummaries_response.json";
 
-function Header() {
+function Header({ name, imgUrl }) {
+  console.log(imgUrl);
   return (
     <div className="flex justify-center w-full py-8">
       <div className="flex container justify-center items-center gap-8 max-w-6xl">
         {/* Profile picture and username */}
-        <img src={mockData.response.players[0].avatarmedium} alt="" />
-        <h1 className="text-3xl text-white">
-          {mockData.response.players[0].personaname}
-        </h1>
+        <img src={imgUrl} alt={name + "'s profile picture"} />
+        <h1 className="text-3xl text-white">{name}</h1>
       </div>
     </div>
   );
--- src/components/stats/StatsCard.jsx
+++ src/components/stats/StatsCard.jsx
@@ -1,24 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import mockGetOwnedGames from "../../mock/getownedgames_response.json";
 import mockChieves from "../../mock/getplayerachievements_response.json";
 import Achievements from "./Achievements";
 import bosses from "./bosses.json";
 
-function StatsCard() {
-  const totalHoursPlayed =
-    mockGetOwnedGames.response.games[0].playtime_forever / 60;
-  const hoursLastTwoWeeks =
-    mockGetOwnedGames.response.games[0].playtime_2weeks / 60;
+function StatsCard({ hours, achievements }) {
+  // console.log(achievements);
 
   let nextBoss = "";
   // Check each boss in order via bosses.json and find that achievement in the response data.
   // If the boss achievement is not achieved, that's the next boss
   for (const [_, boss] of bosses.entries()) {
-    let current = mockChieves.playerstats.achievements.find(
-      (chieve) => chieve.apiname === boss.achievement.apiname
+    let current = achievements.find(
+      (chieve) => chieve.name === boss.achievement.apiname
     );
-    if (!current.achieved) {
+    if (current === undefined) {
       nextBoss = boss.name;
+      console.log(nextBoss);
       break;
     }
   }
@@ -26,17 +25,17 @@ function StatsCard() {
   return (
     <div className="flex flex-col w-full max-w-6xl rounded-md mt-4 px-4 py-2 gap-8">
       <h1 className="text-5xl text-white font-semibold">
-        Total hours played: {totalHoursPlayed}
+        Total hours played: {(hours.playtime_forever / 60).toFixed(1)}
       </h1>
       <h1 className="text-5xl text-white font-semibold">
-        Hours played last 2 weeks: {hoursLastTwoWeeks}
+        Hours played last two weeks: {(hours.playtime_2weeks / 60).toFixed(1)}
       </h1>
       <h1 className="text-5xl text-white font-semibold">
         Next boss: {nextBoss}
       </h1>
       <div className="flex flex-col mt-8">
         <h1 className="text-5xl text-white font-semibold">Achievements</h1>
-        <Achievements />
+        <Achievements data={achievements} />
       </div>
     </div>
   );
